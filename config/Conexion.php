54c2b03037078a28877ae1d500992dbf65c7b8d5
Miruto80 <miguelfernando8012@gmail.com>
2026-04-18 20:08:24 -0400

    Local

--- config/Conexion.php
+++ config/Conexion.php
@@ -11,11 +11,11 @@ class Conexion {
     public function __construct() {
         try {
             // Primera conexión
-            $this->conex1 = new \PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME_1.";charset=utf8", DB_USER, DB_PASS);
+            $this->conex1 = new \PDO("mysql:host=".DB_HOST.";port=3307;dbname=".DB_NAME_1.";charset=utf8", DB_USER, DB_PASS);
             $this->conex1->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
 
             // Segunda conexión
-            $this->conex2 = new \PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME_2.";charset=utf8", DB_USER_2, DB_PASS);
+            $this->conex2 = new \PDO("mysql:host=".DB_HOST.";port=3307;dbname=".DB_NAME_2.";charset=utf8", DB_USER, DB_PASS);
             $this->conex2->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
 
         } catch (\PDOException $e) {
