102a24d9db697e308fb4b29b95f05ab830879ac8
Miruto80 <miguelfernando8012@gmail.com>
2026-06-04 22:14:07 -0400

    Arreglando bd

--- config/config.php
+++ config/config.php
@@ -1,10 +1,10 @@
 <?php
- 
-define('DB_NAME_1', 'lovemakeupbd1');
-define('DB_NAME_2', 'lovemakeupbds2');
+define('DB_NAME_1', 'u417714339_Lovemakeupbd');
+define('DB_NAME_2', 'u417714339_Lovemakeupbds');
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', ''); 
+define('DB_USER', 'u417714339_Lovemakeup');
+define('DB_USER_2', 'u417714339_Lovemakeups');
+define('DB_PASS', 'Labiosfresa123.'); 
 
 date_default_timezone_set('America/Caracas');
 ?>
